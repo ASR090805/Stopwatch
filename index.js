8c3957f2014dd0f685ba8f54c089ee144e3c4e8c
Anuj Rawat <rawatanuj8126@gmail.com>
2026-08-14 22:12:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const display=document.getElementById('display');
+let timer=null;
+let starttime=0;
+let elapsedtime=0;
+let isrunning=false;
+function start(){
+    if(!isrunning){
+    starttime=Date.now()-elapsedtime;
+    timer=setInterval(update,10);
+    isrunning=true;
+    }
+}
+function stop(){
+    if(isrunning){
+        clearInterval(timer);
+        elapsedtime=Date.now()-starttime;
+        isrunning=false;
+    }
+}
+function reset(){
+        clearInterval(timer);
+        starttime=0;
+        elapsedtime=0;
+        isrunning=false;
+        display.textContent="00:00:00:00";
+}
+function update(){
+    const currtime=Date.now();
+    elapsedtime=currtime-starttime;
+     let hours=Math.floor(elapsedtime/(1000*60*60));
+     let minutes=Math.floor(elapsedtime/(1000*60)%60);
+     let seconds=Math.floor(elapsedtime/1000%60);
+     let milisecond=Math.floor(elapsedtime%1000/10);
+     hours=hours.toString().padStart(2,'0');
+     minutes=minutes.toString().padStart(2,'0');
+     seconds=seconds.toString().padStart(2,'0');
+     milisecond=milisecond.toString().padStart(2,'0');
+     
+     display.textContent=`${hours}:${minutes}:${seconds}:${milisecond}`;
+}
